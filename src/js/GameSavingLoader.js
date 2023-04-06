25d37cfeb5d32c7f0b39ad9a29e0533ab845c38f
Mkrtychiyants <heisaloosershesadgmail@gmail.com>
2023-04-06 18:18:51 +0300

    first commit

---
+++ src/js/GameSavingLoader.js
@@ -0,0 +1,27 @@
+import json from './parser.js';
+import read from './reader.js';
+
+export default class GameSavingLoader {
+  // eslint-disable-next-line class-methods-use-this
+  constructor(id, created, userInfo) {
+    this.id = id;
+    this.created = created;
+    this.userInfo = userInfo;
+  }
+
+  static load() {
+    return (async () => {
+      let value;
+      let data;
+      try {
+        data = await read();
+        value = await json(data);
+      } catch (error) {
+        console.log(error);
+      }
+      const saving = JSON.parse(value);
+      const saveObj = new GameSavingLoader(saving.id, saving.created, saving.userInfo);
+      return saveObj;
+    })();
+  }
+}
---
+++ src/js/app.js
@@ -0,0 +1,6 @@
+import GameSavingLoader from './GameSavingLoader.js';
+
+GameSavingLoader.load().then((saving) => {
+  // saving объект класса GameSaving
+  console.log(saving);
+});
