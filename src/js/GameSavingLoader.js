1e45bcc281fb0e6d179efbd181899c060b820462
Mkrtychiyants <heisaloosershesadgmail@gmail.com>
2023-04-13 20:59:05 +0300

    second ed

--- src/js/GameSavingLoader.js
+++ src/js/GameSavingLoader.js
@@ -1,27 +1,17 @@
 import json from './parser';
 import read from './reader';
+import GameSaving from './GameSaving';
 
 export default class GameSavingLoader {
-  // eslint-disable-next-line class-methods-use-this
-  constructor(id, created, userInfo) {
-    this.id = id;
-    this.created = created;
-    this.userInfo = userInfo;
-  }
-
-  static load() {
-    return (async () => {
-      let value;
-      let data;
-      try {
-        data = await read();
-        value = await json(data);
-      } catch (error) {
-        console.log(error);
-      }
+  static async load() {
+    try {
+      const data = await read();
+      const value = await json(data);
       const saving = JSON.parse(value);
-      const saveObj = new GameSavingLoader(saving.id, saving.created, saving.userInfo);
+      const saveObj = new GameSaving(saving.id, saving.created, saving.userInfo);
       return saveObj;
-    })();
+    } catch (error) {
+      return console.log(error);
+    }
   }
 }
